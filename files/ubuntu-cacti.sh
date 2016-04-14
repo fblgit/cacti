42b2c47698290d01e70a6d2bbcaec1f993b04274
FBLGit <xmurias@XAvis-MacBook-Air.local>
2016-04-14 17:49:20 +0800

    F

--- files/ubuntu-cacti.sh
+++ files/ubuntu-cacti.sh
@@ -3,20 +3,25 @@
 DEBIAN_FRONTEND=noninteractive apt-get -qy install mysql-server cacti-spine
 cp /etc/cacti/debian.php /data/debian.php.org
 cp /etc/cacti/spine.conf /data/spine.conf.org
+DB_HOST=${$CACTI_DB_HOST:127.0.0.1}
+DB_PORT=${CACTI_DB_PORT:3306}
+DB_NAME=${CACTI_DB_NAME:cacti}
+DB_USER=${CACTI_DB_USER:cacti}
+DB_PASS=${CACTI_DB_PASSWORD:CACTIDocker}
 echo "<?php
 \$database_type = \"mysql\";
-\$database_default = \"$CACTI_DB_NAME\";
-\$database_hostname = \"$CACTI_DB_HOST\";
-\$database_username = \"$CACTI_DB_USER\";
-\$database_password = \"$CACTI_DB_PASSWORD\";
-\$database_port = \"$CACTI_DB_PORT\";
+\$database_default = \"$DB_NAME\";
+\$database_hostname = \"$DB_HOST\";
+\$database_username = \"$DB_USER\";
+\$database_password = \"$DB_PASS\";
+\$database_port = \"$DB_PORT\";
 \$database_ssl = false;
 ?>">/data/debian.php
-echo "DB_Host         $CACTI_DB_HOST
-DB_Database     $CACTI_DB_NAME
-DB_User         $CACTI_DB_USER
-DB_Pass         $CACTI_DB_PASSWORD
-DB_Port         $CACTI_DB_PORT">/data/spine.conf
+echo "DB_Host         $DB_HOST
+DB_Database     $DB_NAME
+DB_User         $DB_USER
+DB_Pass         $DB_PASS
+DB_Port         $DB_PORT">/data/spine.conf
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 service rsyslog start
 service apache2 start
@@ -26,7 +31,7 @@ if [[ "$CACTI_DB_HOST" -eq "" ]]; then
     sleep 5
     cp /data/spine.conf /etc/cacti/
     cp /data/debian.php /etc/cacti/
-    GRANT="GRANT ALL PRIVILEGES ON $CACTI_DB_NAME.* TO $CACTI_DB_USER@% IDENTIFIED BY '$CACTI_DB_PASSWORD';"
+    GRANT="GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@% IDENTIFIED BY '$DB_PASS';"
     echo $GRANT
     mysql << EOF
 $GRANT
