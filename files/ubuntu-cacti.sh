7b9d36376a87803ac820f233d1649c01d3b42d63
FBLGit <xmurias@XAvis-MacBook-Air.local>
2016-04-14 18:13:49 +0800

    Final

--- files/ubuntu-cacti.sh
+++ files/ubuntu-cacti.sh
@@ -3,7 +3,7 @@
 DEBIAN_FRONTEND=noninteractive apt-get -qy install mysql-server apache2
 cp /etc/cacti/debian.php /data/debian.php.org
 cp /etc/cacti/spine.conf /data/spine.conf.org
-DB_HOST=${$CACTI_DB_HOST:-127.0.0.1}
+DB_HOST=${$CACTI_DB_HOST:-localhost}
 DB_PORT=${CACTI_DB_PORT:-3306}
 DB_NAME=${CACTI_DB_NAME:-cacti}
 DB_USER=${CACTI_DB_USER:-cacti}
@@ -26,22 +26,8 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 service rsyslog start
 service mysql start
 sleep 3
-service apache2 start
 DEBIAN_FRONTEND=noninteractive apt-get -qy install cacti-spine
-if [[ "$CACTI_DB_HOST" -eq "" ]]; then
-  if [[ ! -f /data/.granted ]] ; then
-    cp /data/spine.conf /etc/cacti/
-    cp /data/debian.php /etc/cacti/
-    GRANT="GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-    echo $GRANT
-    mysql << EOF
-CREATE DATABASE cacti;
-$GRANT
-FLUSH PRIVILEGES;
-EOF
-    touch /data/.granted
-  fi
-fi
+service apache2 start
 echo "Deliver to Cron"
 cron -f
 echo "Cron Running"
